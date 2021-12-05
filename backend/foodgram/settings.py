e2cf785026194e2a23142d7d50c887e8e69586ab
Vladislav <v.butirsky@gmail.com>
2021-12-06 01:31:10 +0700

    v1.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    "84.252.138.253",
     "localhost",
     "127.0.0.1",
     "[::1]",
